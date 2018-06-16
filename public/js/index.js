7478f696e42a769e6c7a85d84bfce7881ff75569
hiroshi <hiroshi@iMacHiroshi2.local>
2018-06-16 23:15:14 +0900

    mod

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,25 @@
-var selected_color = "transparent";
-const PALLETS = [
-    { id: "pallet0", color: "transparent" },
-    { id: "pallet1", color: "white" },
-    { id: "pallet2", color: "red" },
-    { id: "pallet3", color: "yellow" },
-    { id: "pallet4", color: "lightgreen" },
-    { id: "pallet5", color: "aqua" },
-    { id: "pallet6", color: "blue" },
-    { id: "pallet7", color: "pink" },
-    { id: "pallet8", color: "violet" }
-];
+var selected_pallet;
+var current_cells; // Array [16][32]
+const PALLETS = {
+    pallet0: { color: "transparent", unselected: "black", selected: "red" },
+    pallet1: { color: "white", unselected: "black", selected: "red" },
+    pallet2: { color: "red", unselected: "black", selected: "blue" },
+    pallet3: { color: "yellow", unselected: "black", selected: "red" },
+    pallet4: { color: "lightgreen", unselected: "black", selected: "red" },
+    pallet5: { color: "aqua", unselected: "black", selected: "red" },
+    pallet6: { color: "blue", unselected: "black", selected: "red" },
+    pallet7: { color: "pink", unselected: "black", selected: "red" },
+    pallet8: { color: "violet", unselected: "black", selected: "red" }
+};
 const CELL_WIDTH = 16;
 const CELL_HEIGHT = 16;
 
-const clickPallet = color => {
-    selected_color = color;
+const setPallet = pallet => {
+    selected_pallet = pallet;
+    for(let id in PALLETS){
+        const border_color = id === selected_pallet? PALLETS[id].selected : PALLETS[id].unselected;
+        $("#" + id).css("border-color", border_color);
+    }
 }
 const updateWindow = () => {
     $(".cell").css("width", CELL_WIDTH).css("height", CELL_HEIGHT);
@@ -22,19 +27,22 @@ const updateWindow = () => {
     const left = ($(window).width() - $("#main").width()) / 2;
     $("#main").css("margin-top", top).css("margin-left", left);
 }
+const setCell = (x, y, pallet) => {
+    const id = "#cell_" + x + "_" + y;
+    $(id).css("background-color", PALLETS[pallet].color);
+    current_cells[x][y] = PALLETS[pallet];
+}
 const updateCellColor = event => {
     const p0 = $("#cells").offset();
     const p1 = event.changedTouches[0];
     const x = Math.floor((p1.pageX - p0.left) / (CELL_WIDTH + 3.6));
     const y = Math.floor((p1.pageY - p0.top) / (CELL_HEIGHT + 3.6));
-    const id = "#cell_" + x + "_" + y;
-    $(id).css("background-color", selected_color);
+    setCell(x, y, selected_pallet);
 }
 const clearCells = () => {
     for(var x = 0; x < 16; ++x){
         for(var y = 0; y < 32; ++y){
-            const id = "#cell_" + x + "_" + y;
-            $(id).css("background-color", "transparent");
+            setCell(x, y, "pallet0");
         }
     }
 }
@@ -48,15 +56,21 @@ $(document).ready(() => {
     $(window).resize(() => {
         updateWindow();
     });
+    current_cells = new Array(16);
+    for(let x = 0; x < current_cells.length; ++x) {
+        current_cells[x] = new Array(32).fill(0);
+    }
     clearCells();
     $("#trash").click(() => clearCells());
     $("#transparent").click(() => clickPallet("transparent"));
-    PALLETS.forEach(pallet => {
-        var obj = $("#" + pallet.id);
-        obj.addClass("pallet").on("click", () => clickPallet(pallet.color)).css("background-color", pallet.color);
+    for(let id in PALLETS){
+        var obj = $("#" + id);
+        const pallet = PALLETS[id];
+        obj.addClass("pallet").on("click", () => setPallet(id)).css("background-color", pallet.color);
         if(pallet.color === "transparent"){
             const img = $("<img>").attr("border", 0).attr("src", "assets/eraser.png").attr("width", "50px").attr("height", "50px");
             obj.css("background-color", "lightgray").append(img);
         }
-    });
+    }
+    setPallet("pallet0");
 });
