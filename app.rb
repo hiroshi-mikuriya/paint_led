0fb076ea18c80ef6e979cf4a8e8a291bca237176
hiroshi <hiroshi@iMacHiroshi2.local>
2018-06-16 23:24:54 +0900

    add fiddle

--- app.rb
+++ app.rb
@@ -4,6 +4,22 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 require 'socket'
+require 'fiddle/import'
+
+##
+# load 3D LED CUBE SDK
+module LED
+  extend Fiddle::Importer
+  dlload './libledLib.dylib'
+  extern 'void SetUrl(char *)'
+  extern 'void SetLed(int, int, int, int)'
+  extern 'void Clear()'
+  extern 'void Show()'
+  extern 'void Wait(int)'
+  extern 'void ShowMotioningText1(char *)'
+  extern 'void SetChar(int, int, int, char, int)'
+  extern 'void ShowFirework(int, int, int)'
+end
 
 ##
 # Server program
