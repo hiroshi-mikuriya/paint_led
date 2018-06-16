db99740106bd64df83c13bc6d239614be1100542
hiroshi <hiroshi@iMacHiroshi2.local>
2018-06-16 17:30:12 +0900

    use trash image

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,21 @@
-var selected_color = "black";
-const CELL_WIDTH = 18;
-const CELL_HEIGHT = 18;
+var selected_color = "transparents";
+const CELL_WIDTH = 16;
+const CELL_HEIGHT = 16;
 
-const clickPallet = (id) => {
-    selected_color = id;
+const clickPallet = (color) => {
+    selected_color = color;
 }
 const updateWindow = () => {
     $(".cell").css("width", CELL_WIDTH).css("height", CELL_HEIGHT);
     const top = ($(window).height() - $("#main").height()) / 2;
     const left = ($(window).width() - $("#main").width()) / 2;
     $("#main").css("margin-top", top).css("margin-left", left);
-    $("#clear").css("height", "50px").css("width", "100px").text("やりなおし");
 }
 const updateCellColor = (event) => {
     const p0 = $("#cells").offset();
     const p1 = event.changedTouches[0];
-    const x = Math.floor((p1.pageX - p0.left) / (CELL_WIDTH + 1.2));
-    const y = Math.floor((p1.pageY - p0.top) / (CELL_HEIGHT + 1.2));
+    const x = Math.floor((p1.pageX - p0.left) / (CELL_WIDTH + 3.6));
+    const y = Math.floor((p1.pageY - p0.top) / (CELL_HEIGHT + 3.6));
     const id = "#cell_" + x + "_" + y;
     $(id).css("background-color", selected_color);
 }
@@ -24,7 +23,7 @@ const clearCells = () => {
     for(var x = 0; x < 16; ++x){
         for(var y = 0; y < 32; ++y){
             const id = "#cell_" + x + "_" + y;
-            $(id).css("background-color", "black");
+            $(id).css("background-color", "transparents");
         }
     }
 }
@@ -39,5 +38,6 @@ $(document).ready(() => {
         updateWindow();
     });
     clearCells();
-    $("#clear").on("click", () => clearCells());
+    $("#trash").click(() => clearCells());
+    $("#eraser").click(() => clickPallet("transparents"));
 });
