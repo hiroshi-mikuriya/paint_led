acc624956c8574dfb825ac20d1d6ba05c59ba52d
hiroshi <hiroshi@iMacHiroshi2.local>
2018-06-18 22:27:14 +0900

    mod fiddle

--- led.rb
+++ led.rb
@@ -1,10 +1,49 @@
+# frozen_string_literal: true
+
+require 'rbconfig'
 require 'fiddle/import'
 
-##
-# load 3D LED CUBE SDK
+def os
+  @os ||= (
+    host_os = RbConfig::CONFIG['host_os']
+    case host_os
+    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+      :windows
+    when /darwin|mac os/
+      :macosx
+    when /linux/
+      :linux
+    when /solaris|bsd/
+      :unix
+    else
+      :unknown
+    end
+  )
+end
+
 module LED
   extend Fiddle::Importer
-  dlload './libledLib.dylib'
+
+  dirname = File.dirname(__FILE__)
+  case os
+  when :macosx
+    dlload dirname + '/libledLib.dylib'
+  when :windows
+    if RbConfig::CONFIG['target_cpu'] == 'i386' then
+      dlload dirname + '/ledLib32.dll'
+    else
+      dlload dirname + '/ledLib64.dll'
+    end
+  when :linux
+    if `uname -m`.strip == 'armv7l' then
+      dlload dirname + '/ledLibarmv7l.so'  # for Raspberry PI3
+    else
+      raise RuntimeError
+    end
+  else
+    raise RuntimeError
+  end
+
   extern 'void SetUrl(char *)'
   extern 'void SetPort(short)'
   extern 'void SetLed(int, int, int, int)'
